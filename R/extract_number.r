01e23954c9570ef258e98dc2aafdfd8fce1ffce8
Tim Bergsma <tim.bergsma@qpharmetra.com>
2025-06-30 23:18:10 -0400

    bugfix

--- R/extract_number.r
+++ R/extract_number.r
@@ -10,10 +10,18 @@
 #' extract_number("c76")
 #' extract_number("c76c7")
 #' extract_number("BLOQ = 0.144")
+#' extract_number("6.023+23")
+#' stopifnot(extract_number("6.1973+01") == 61.973)
+#' extract_number(c('7e1', '7e+1', '7E-1'))
+#' extract_number(c('-7e1', '+7e+1', '-7E-1'))
+#' extract_number('E7')
 
 extract_number <- function(x)
 {
   x = as.character(x)
-  w1 = regexpr("[+-]?[0-9.]+", x)
+  # 2025-06-30 TTB
+  # shrinkage in the form 6.1973+01 is not recognized by as.numeric
+  x <- sub('(\\d)([+-]\\d+).*$','\\1E\\2', x)
+  w1 = regexpr("[+-]?[0-9.]+(?:[eE][+-]?\\d+)?", x)
   as.numeric(substring(x, w1, w1 + attr(w1, "match.length")-1))
 }
